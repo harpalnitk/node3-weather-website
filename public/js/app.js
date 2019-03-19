25156e860bfe33abee41a38156fe8034eee335e1
Harpal Singh <harpalnitk@gmail.com>
2019-03-19 19:07:49 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e)=> {
     resultLocation.innerHTML = '';
     resultForecast.innerHTML = '';
     const location = search.value;
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 resultAddress.innerHTML = data.error;
--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 //Define paths for Express config
 const viewsPath = path.join(__dirname, '../templates/views');
 const publicDirectory = path.join(__dirname, '../public');
@@ -88,6 +89,6 @@ app.get('*', (req,res)=> {
     });
 });
 
-app.listen(3000, ()=> {
-    console.log(chalk.green.inverse('Server is running on port 3000!'));
-})
+app.listen(port, ()=> {
+    console.log(chalk.green.inverse('Server is running on port'+ port));
+});
